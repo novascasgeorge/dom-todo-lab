84261fe2b4c7a1d6e9798b7b0fcbc3f870868198
User Name <itsthegeorges@gmail.com>
2022-08-31 08:50:53 -0400

    labfinish

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('sanity check')
+const titleElement = document.getElementById('main-title')
+// title element
+
+// cached element references
+
+const btn = document.querySelector('button')
+const input = document.querySelector('input')
+document.querySelector('ul').addEventListener(`click`, 
+handleClick)
+//console.log(input)
+
+
+function handleClick(evt){
+ // console.log(evt.target)
+}
+
+btn.addEventListener('click', function(evt) {
+  // create <li> element
+  const li = document.createElement('li')
+  //access the text from the input
+  // store the text on the <li>
+  li.textContent = input.value
+// console.log(li)
+  //add the <li>(with the text included) to html(in the <ul>)
+document.querySelector('ul').appendChild(li)
+input.value = ""
+});
